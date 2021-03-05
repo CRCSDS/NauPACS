99ad93ed15bc319f38371f47414d198e424958a7
JavierCopete <javier.copete@sarria.salesians.cat>
2021-03-05 16:11:40 +0100

    05/03/21

---
+++ Planeta/PlanetaPACS/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿
+namespace PlanetaPACS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_codi = new System.Windows.Forms.Button();
+            this.cmb_Planeta = new System.Windows.Forms.ComboBox();
+            this.btn_clau = new System.Windows.Forms.Button();
+            this.btn_Servidor = new System.Windows.Forms.Button();
+            this.btn_Desconectar = new System.Windows.Forms.Button();
+            this.btn_Client = new System.Windows.Forms.Button();
+            this.btn_DesconectarClient = new System.Windows.Forms.Button();
+            this.lb_Missatges = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_Missatge = new System.Windows.Forms.Label();
+            this.btn_xifrat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_codi
+            // 
+            this.btn_codi.Font = new System.Drawing.Font("Corbel", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_codi.Location = new System.Drawing.Point(200, 51);
+            this.btn_codi.Name = "btn_codi";
+            this.btn_codi.Size = new System.Drawing.Size(114, 64);
+            this.btn_codi.TabIndex = 0;
+            this.btn_codi.Text = "Generar codi";
+            this.btn_codi.UseVisualStyleBackColor = true;
+            this.btn_codi.Click += new System.EventHandler(this.btn_codi_Click);
+            // 
+            // cmb_Planeta
+            // 
+            this.cmb_Planeta.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Planeta.FormattingEnabled = true;
+            this.cmb_Planeta.Location = new System.Drawing.Point(40, 51);
+            this.cmb_Planeta.Name = "cmb_Planeta";
+            this.cmb_Planeta.Size = new System.Drawing.Size(140, 29);
+            this.cmb_Planeta.Sorted = true;
+            this.cmb_Planeta.TabIndex = 1;
+            // 
+            // btn_clau
+            // 
+            this.btn_clau.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clau.Location = new System.Drawing.Point(320, 51);
+            this.btn_clau.Name = "btn_clau";
+            this.btn_clau.Size = new System.Drawing.Size(114, 64);
+            this.btn_clau.TabIndex = 2;
+            this.btn_clau.Text = "Generar clau";
+            this.btn_clau.UseVisualStyleBackColor = true;
+            this.btn_clau.Click += new System.EventHandler(this.btn_clau_Click);
+            // 
+            // btn_Servidor
+            // 
+            this.btn_Servidor.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Servidor.Location = new System.Drawing.Point(827, 34);
+            this.btn_Servidor.Name = "btn_Servidor";
+            this.btn_Servidor.Size = new System.Drawing.Size(140, 85);
+            this.btn_Servidor.TabIndex = 4;
+            this.btn_Servidor.Text = "Iniciar Servidor";
+            this.btn_Servidor.UseVisualStyleBackColor = true;
+            this.btn_Servidor.Click += new System.EventHandler(this.btn_Servidor_Click);
+            // 
+            // btn_Desconectar
+            // 
+            this.btn_Desconectar.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Desconectar.Location = new System.Drawing.Point(827, 125);
+            this.btn_Desconectar.Name = "btn_Desconectar";
+            this.btn_Desconectar.Size = new System.Drawing.Size(140, 85);
+            this.btn_Desconectar.TabIndex = 5;
+            this.btn_Desconectar.Text = "Desconectar Servidor";
+            this.btn_Desconectar.UseVisualStyleBackColor = true;
+            this.btn_Desconectar.Click += new System.EventHandler(this.btn_Desconectar_Click);
+            // 
+            // btn_Client
+            // 
+            this.btn_Client.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Client.Location = new System.Drawing.Point(827, 216);
+            this.btn_Client.Name = "btn_Client";
+            this.btn_Client.Size = new System.Drawing.Size(140, 85);
+            this.btn_Client.TabIndex = 7;
+            this.btn_Client.Text = "Iniciar Client";
+            this.btn_Client.UseVisualStyleBackColor = true;
+            this.btn_Client.Click += new System.EventHandler(this.btn_Client_Click);
+            // 
+            // btn_DesconectarClient
+            // 
+            this.btn_DesconectarClient.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DesconectarClient.Location = new System.Drawing.Point(827, 307);
+            this.btn_DesconectarClient.Name = "btn_DesconectarClient";
+            this.btn_DesconectarClient.Size = new System.Drawing.Size(140, 85);
+            this.btn_DesconectarClient.TabIndex = 8;
+            this.btn_DesconectarClient.Text = "Desconectar Client";
+            this.btn_DesconectarClient.UseVisualStyleBackColor = true;
+            this.btn_DesconectarClient.Click += new System.EventHandler(this.btn_DesconectarClient_Click);
+            // 
+            // lb_Missatges
+            // 
+            this.lb_Missatges.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lb_Missatges.FormattingEnabled = true;
+            this.lb_Missatges.ItemHeight = 16;
+            this.lb_Missatges.Location = new System.Drawing.Point(982, 34);
+            this.lb_Missatges.Name = "lb_Missatges";
+            this.lb_Missatges.Size = new System.Drawing.Size(400, 260);
+            this.lb_Missatges.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(982, 331);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 61);
+            this.textBox1.TabIndex = 10;
+            // 
+            // lbl_Missatge
+            // 
+            this.lbl_Missatge.AutoSize = true;
+            this.lbl_Missatge.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Missatge.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lbl_Missatge.Location = new System.Drawing.Point(978, 307);
+            this.lbl_Missatge.Name = "lbl_Missatge";
+            this.lbl_Missatge.Size = new System.Drawing.Size(75, 21);
+            this.lbl_Missatge.TabIndex = 11;
+            this.lbl_Missatge.Text = "Missatge";
+            // 
+            // btn_xifrat
+            // 
+            this.btn_xifrat.Font = new System.Drawing.Font("Corbel", 10.8F);
+            this.btn_xifrat.Location = new System.Drawing.Point(200, 145);
+            this.btn_xifrat.Name = "btn_xifrat";
+            this.btn_xifrat.Size = new System.Drawing.Size(114, 65);
+            this.btn_xifrat.TabIndex = 12;
+            this.btn_xifrat.Text = "Generar Xifrat";
+            this.btn_xifrat.UseVisualStyleBackColor = true;
+            this.btn_xifrat.Click += new System.EventHandler(this.btn_xifrat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1412, 753);
+            this.Controls.Add(this.btn_xifrat);
+            this.Controls.Add(this.lbl_Missatge);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lb_Missatges);
+            this.Controls.Add(this.btn_DesconectarClient);
+            this.Controls.Add(this.btn_Client);
+            this.Controls.Add(this.btn_Desconectar);
+            this.Controls.Add(this.btn_Servidor);
+            this.Controls.Add(this.btn_clau);
+            this.Controls.Add(this.cmb_Planeta);
+            this.Controls.Add(this.btn_codi);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PlanetaPACS";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_codi;
+        private System.Windows.Forms.ComboBox cmb_Planeta;
+        private System.Windows.Forms.Button btn_clau;
+        private System.Windows.Forms.Button btn_Servidor;
+        private System.Windows.Forms.Button btn_Desconectar;
+        private System.Windows.Forms.Button btn_Client;
+        private System.Windows.Forms.Button btn_DesconectarClient;
+        private System.Windows.Forms.ListBox lb_Missatges;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_Missatge;
+        private System.Windows.Forms.Button btn_xifrat;
+    }
+}
+
---
+++ Planeta/PlanetaPACS/Form1.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PACS_Dades;
+
+namespace PlanetaPACS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private static Dades bbdd = new Dades();
+        string code;
+        int port = 4500;
+        char[] diccionari = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataSet dts = bbdd.PortarTaula("Planets");
+
+            cmb_Planeta.DataSource = dts.Tables[0];
+            cmb_Planeta.ValueMember = "idPlanet";
+            cmb_Planeta.DisplayMember = "DescPlanet";
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void btn_codi_Click(object sender, EventArgs e)
+        {
+
+            byte[] data = new byte[4 * diccionari.Length];
+            using (RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider())
+            {
+                crypto.GetBytes(data);
+            }
+            StringBuilder result = new StringBuilder(diccionari.Length);
+            for (int i = 0; i < 12; i++)
+            {
+                var random = BitConverter.ToUInt32(data, i * 4);
+                var numero = random % diccionari.Length;
+
+                result.Append(diccionari[numero]);
+            }
+
+            code = result.ToString();
+
+            string idPlanet = Portar_idPlanet();
+
+            DataSet dtsEncryption = bbdd.PortarTaula("InnerEncryption");
+            string queryEncryption = "select * from InnerEncryption where idPlanet = '" + idPlanet + "'";
+
+            DataSet dtsidEncryption = bbdd.PortarPerConsulta(queryEncryption);
+
+            if (dtsidEncryption.Tables[0].Rows.Count > 0)
+            {
+                DataRow drEncryption = dtsidEncryption.Tables[0].Rows[0];
+                drEncryption["ValidationCode"] = code;
+
+                bbdd.Actualitzar(dtsidEncryption, queryEncryption);
+            }
+            else
+            {
+                DataRow drEncryption = dtsEncryption.Tables[0].NewRow();
+                drEncryption["idPlanet"] = idPlanet;
+                drEncryption["ValidationCode"] = code;
+
+                dtsEncryption.Tables[0].Rows.Add(drEncryption);
+                string query = "select * from InnerEncryption";
+                bbdd.Actualitzar(dtsEncryption, query);
+            }
+        }
+
+        private void btn_clau_Click(object sender, EventArgs e)
+        {
+            string planeta = cmb_Planeta.Text;
+            string queryPlaneta = "select idPlanet from Planets where DescPlanet = '" + planeta + "'";
+            DataSet dtsPlaneta = bbdd.PortarPerConsulta(queryPlaneta);
+            string idPlanet = dtsPlaneta.Tables[0].Rows[0]["idPlanet"].ToString();
+
+            string QueryValidation = "select ValidationCode from InnerEncryption where idPlanet = '" + idPlanet + "'";
+            DataSet dtsValidation = bbdd.PortarPerConsulta(QueryValidation);
+            string ValidationCode = dtsValidation.Tables[0].Rows[0]["ValidationCode"].ToString();
+
+            CspParameters cspp = new CspParameters();
+            string keyName = ValidationCode;
+            cspp.KeyContainerName = keyName;
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(cspp);
+            RSAParameters RSAKeyInfo = rsa.ExportParameters(false);
+
+            string publicKey = rsa.ToXmlString(false);
+            string privateKey = rsa.ToXmlString(true);
+            rsa.PersistKeyInCsp = true;
+
+            DataSet dtsPlanetKeys = bbdd.PortarTaula("PlanetKeys");
+            string queryPlanetKeys = "select * from PlanetKeys where idPlanet = '" + idPlanet + "'";
+            DataSet dtsidPlanetKeys = bbdd.PortarPerConsulta(queryPlanetKeys);
+
+            if (dtsidPlanetKeys.Tables[0].Rows.Count > 0)
+            {
+                DataRow drPlanetKeys = dtsidPlanetKeys.Tables[0].Rows[0];
+                drPlanetKeys["XMLKey"] = publicKey;
+
+                bbdd.Actualitzar(dtsidPlanetKeys, queryPlanetKeys);
+            }
+            else
+            {
+                DataRow drPlanetKeys = dtsPlanetKeys.Tables[0].NewRow();
+                drPlanetKeys["idPlanet"] = idPlanet;
+                drPlanetKeys["XMLKey"] = publicKey;
+
+                dtsPlanetKeys.Tables[0].Rows.Add(drPlanetKeys);
+                string query = "select * from PlanetKeys";
+                bbdd.Actualitzar(dtsPlanetKeys, query);
+            }
+        }
+
+        Thread hilo;
+        Boolean servidor = true;
+        Boolean conectat = false;
+        TcpListener Listener;
+        TcpClient client;
+        NetworkStream ns;
+
+        private void btn_Servidor_Click(object sender, EventArgs e)
+        {
+            if (!conectat)
+            {
+                lb_Missatges.Items.Add("Servidor Conectat");
+                conectat = true;
+                servidor = true;
+                hilo = new Thread(function);
+                hilo.Start();
+            }
+        }
+        private void function()
+        {
+            Listener = new TcpListener(IPAddress.Any, port);
+            Listener.Start();
+
+            while (servidor)
+            {
+                if (Listener.Pending())
+                {
+                    client = Listener.AcceptTcpClient();
+                    ns = client.GetStream();
+                    byte[] buffer = new byte[1024];
+                    ns.Read(buffer, 0, buffer.Length);
+                    lb_Missatges.Items.Add(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString() + " ha enviat: ");
+                    lb_Missatges.Items.Add(Encoding.UTF8.GetString(buffer));
+                }
+            }
+        }
+
+        private void btn_Desconectar_Click(object sender, EventArgs e)
+        {
+            lb_Missatges.Items.Add("Servidor Desconectat");
+            conectat = false;
+            if (hilo != null)
+            {
+                servidor = false;
+                hilo.Abort();
+                Listener.Stop();
+                if (ns != null)
+                {
+                    ns.Close();
+                }
+                if (client != null)
+                {
+                    client.Close();
+                }
+            }
+        }
+
+        private void btn_Client_Click(object sender, EventArgs e)
+        {
+            lb_Missatges.Items.Add("Client Iniciat");
+        }
+
+        private void btn_DesconectarClient_Click(object sender, EventArgs e)
+        {
+            lb_Missatges.Items.Add("Client Desconectat");
+        }
+
+        private void btn_xifrat_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, string> Xifrat = new Dictionary<string, string>();
+            Random rnd = new Random();
+            string num_rnd_string;
+            Boolean a = true;
+            string idPlanet = Portar_idPlanet();
+
+            for (char l = 'A'; l < 'Z' + 1; l++)
+            {
+                int num_rnd = rnd.Next(999);
+
+                while (Xifrat.ContainsValue(num_rnd.ToString()))
+                {
+                    num_rnd = rnd.Next(999);
+                }
+
+                num_rnd_string = num_rnd.ToString();
+
+                if (num_rnd < 100 && num_rnd >= 0)
+                {
+                    num_rnd_string = num_rnd_string.PadLeft(3, '0');
+                }
+
+                Xifrat.Add(l.ToString(), num_rnd_string);
+
+                DataSet dtsxifrat = bbdd.PortarTaula("InnerEncryptionData");
+                DataRow drxifrat = dtsxifrat.Tables[0].NewRow();
+
+                string queryid = "select * from InnerEncryption where idPlanet = '" + idPlanet + "'";
+                DataSet dtsIdInnerEncryption = bbdd.PortarPerConsulta(queryid);
+                string IdInnerEncryption = dtsIdInnerEncryption.Tables[0].Rows[0]["idInnerEncryption"].ToString();
+
+                if (dtsIdInnerEncryption.Tables[0].Rows.Count > 0 && a)
+                {
+                    string queryDelete = "delete from InnerEncryptionData where IdInnerEncryption = '" + IdInnerEncryption + "'";
+                    bbdd.Executa(queryDelete);
+                    a = false;
+                }
+
+                drxifrat["IdInnerEncryption"] = IdInnerEncryption;
+                drxifrat["Word"] = l.ToString();
+                drxifrat["Numbers"] = num_rnd_string;
+                dtsxifrat.Tables[0].Rows.Add(drxifrat);
+
+                string query = "select * from InnerEncryptionData";
+                bbdd.Actualitzar(dtsxifrat, query);
+
+            }
+            lb_Missatges.Items.Add("Diccionari generat correctament");
+        }
+
+        private string Portar_idPlanet()
+        {
+            string planeta = cmb_Planeta.Text;
+            string queryPlaneta = "select idPlanet from Planets where DescPlanet = '" + planeta + "'";
+            DataSet dtsPlaneta = bbdd.PortarPerConsulta(queryPlaneta);
+
+            string idPlanet = dtsPlaneta.Tables[0].Rows[0]["idPlanet"].ToString();
+
+            return idPlanet;
+        }
+    }
+}
